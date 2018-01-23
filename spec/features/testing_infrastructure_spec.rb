3b0bcad5586fd8fc3315ac255649a3bae0dba359
Ed Goold <teddy_goold@hotmail.com>
2018-01-23 11:17:53 +0000

    updted initial feature test

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,15 @@
+# require 'link'
 
-feature 'homepage' do
-  scenario 'see list of links' do
-    visit('/')
-    Link.create(link: "www.bbc.co.uk")
-    expect(page).to have_content(link)
-  end
+feature 'viewing links' do
+    Link.create(url: "www.bbc.co.uk", title: "bbc homepage")
+    scenario 'upon visiting the homepage, see list of links' do
+
+      visit('/')
+
+      expect(page.status_code).to eq 200
+
+      within 'ul#links' do
+        expect(page).to have_content("bbc homepage")
+      end
+    end
 end
